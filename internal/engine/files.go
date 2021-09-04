b8eb5b4c40bdef7a7fa183956d94087651d0d302
mauricio riquelme <m.riquelme223@gmail.com>
2021-09-04 15:21:55 +1000

    need to revisit this

--- internal/engine/files.go
+++ internal/engine/files.go
@@ -16,13 +16,19 @@ func (w *writer) WriteFile(name string, data []byte, perm fs.FileMode) error {
 func (w *writer) AppendFile(name string, data []byte) error {
 	w.mx.Lock()
 	defer w.mx.Unlock()
+	fileData, err := os.ReadFile(name)
+	if err != nil {
+		log.Println("error reading file", err)
+		return err
+	}
 	file, err := os.OpenFile(name, os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Println("error opening file", err)
 		return err
 	}
 	defer file.Close()
-	if _, err := file.WriteString(string(data)); err != nil {
+	sourceData := append(fileData, data...)
+	if _, err := file.Write(sourceData); err != nil {
 		log.Println("error appending data", err)
 		return err
 	}
