45b4f8b3686108f955381128271f33c981c5d00d
mauricio riquelme <m.riquelme223@gmail.com>
2022-01-09 18:59:19 +1100

    cleaning

--- internal/writer/logs.go
+++ internal/writer/logs.go
@@ -15,7 +15,7 @@ func (f *fileLog) WriteFile(name string, data []byte, perm os.FileMode) error {
 	return nil
 }
 func (f *fileLog) ReadFile(name string) ([]byte, error) {
-	return os.ReadFile(name)
+	return os.ReadFile(filepath.Base(name))
 }
 
 func (f *fileLog) OpenFile(name string, flag int, perm fs.FileMode) (*os.File, error) {
