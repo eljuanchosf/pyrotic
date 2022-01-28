39089fcb726383bd19db39133c6a803a990d95de
mauricio riquelme <m.riquelme223@gmail.com>
2022-01-28 18:08:14 +1100

    adding template

--- internal/formats/stringers.go
+++ internal/formats/stringers.go
@@ -10,14 +10,14 @@ func SplitAfterDelimiter(value string, delimiter string) []string {
 	return strings.SplitAfter(value, delimiter)
 }
 
-func Contains(value string, subStr string, n int) bool {
+func Contains(value string, subStr string) bool {
 	return strings.Contains(value, subStr)
 }
 
-func HasPrefix(value string, prefix string, n int) bool {
+func HasPrefix(value string, prefix string) bool {
 	return strings.HasPrefix(value, prefix)
 }
 
-func HasSuffix(value string, suffix string, n int) bool {
+func HasSuffix(value string, suffix string) bool {
 	return strings.HasSuffix(value, suffix)
 }
